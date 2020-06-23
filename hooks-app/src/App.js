a5eef2fd23f12747beb785d44066b7cdf8c59163
Marie Antons <chefantons@gmail.com>
2020-06-22 20:26:28 -0500

    create and update complete

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 
 
-
 import './app.scss';
 
 
@@ -24,6 +23,29 @@ function App() {
     }
   ]);
 
+ function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodoAtIndex(e, i);
+    }
+  }
+  //creation of todo input box
+ function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+  //Update the ToDo
+  function updateTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
   
   return (
     <body>
@@ -35,7 +57,7 @@ function App() {
         {todos.map((todo, i) => (
       <div className="todo">
         <div className="checkbox" />
-        <input type="text" value={todo.content} onKeyDown = {e => handleKeyDown(e,i)} />
+        <input type="text" value={todo.content} onKeyDown = {e => handleKeyDown(e,i)}  onChange={e => updateTodoAtIndex(e, i)}/>
       </div>
     ))}
         </ul>
