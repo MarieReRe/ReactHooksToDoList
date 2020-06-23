ba4d0c5f075ec867ef97b14780115537a5cce886
Marie Antons <chefantons@gmail.com>
2020-06-22 20:40:29 -0500

    deletion of a todo

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -27,6 +27,10 @@ function App() {
     if (e.key === 'Enter') {
       createTodoAtIndex(e, i);
     }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodoAtIndex(i);
+    }
   }
   //creation of todo input box
  function createTodoAtIndex(e, i) {
@@ -40,11 +44,19 @@ function App() {
       document.forms[0].elements[i + 1].focus();
     }, 0);
   }
-  //Update the ToDo
+  //Update the ToDo- what actually lets you type
   function updateTodoAtIndex(e, i) {
     const newTodos = [...todos];
     newTodos[i].content = e.target.value;
     setTodos(newTodos);
+  }
+  //Remove
+  function removeTodoAtIndex(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
   }
   
   return (
