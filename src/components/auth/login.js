52cbe0fd645ccbb8df051ca90743bafc9a297ee7
Marie Antons <chefantons@gmail.com>
2020-06-25 10:37:00 -0500

    added handle submit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -3,4 +3,10 @@ import React from 'react';
 //make this a function compoment
 export default function Login() {
 
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        Login();
+      }
+
 }
