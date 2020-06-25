3cb971c6b7eb66edf52ae4185f2dbff40fbb6ff7
Marie Antons <chefantons@gmail.com>
2020-06-25 10:59:47 -0500

    start of user login form

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { render } from 'node-sass';
 
 //make this a function compoment
 export default function Login() {
 
-
+    //static contextType = AuthContext;
     handleSubmit = (event) => {
         event.preventDefault();
+        const { username, password } = e.target.elements;
         Login();
-      }
-
+    }
+ 
+        return (
+            <div className="Login">
+                <form onSubmit={handleSubmit}>
+                    <input placeholder="Username" ></input>
+                    <input placeholder="Password" ></input>
+                </form>
+            </div>
+        )
 }
+
+
